69ec92830b6fc218e317ee47904baa4064f555f9
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2022-02-25 13:47:16 +0900

    posts-save 타임리프 전환

--- src/main/java/hello/login/web/mail/MailController.java
+++ src/main/java/hello/login/web/mail/MailController.java
@@ -3,6 +3,7 @@ package hello.login.web.mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,8 @@ public class MailController {
 	}
 
 	@GetMapping("/posts/save")
-	public String postsSave() {
+	public String postsSave(Model model) {
+		model.addAttribute("MailSaveDto", new MailSaveDto());
 		return "posts-save";
 	}
 
